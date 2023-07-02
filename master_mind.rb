1af8b0a58d7d53886c48148117654a61dea84979
lucasmatheus001 <lucasmatheussm22@gmail.com>
2023-07-02 17:46:11 -0300

    the user can say the number of rounds

--- master_mind.rb
+++ master_mind.rb
@@ -1,6 +1,7 @@
 @cor =  [1,2,3,4,5,6]
+@cor.shuffle!
+
 class Master_mind
-  
   
   def verifica_cor_posicao(cor,cor2)
     cont =0
@@ -22,6 +23,15 @@ class Master_mind
   end
 end
 
-cor = [1,3,2,25,9,20]
+# start interaction
 master = Master_mind.new
-master.verifica_cor_posicao(cor,@cor)
+rodadas =0
+puts "-------------------------------- Inciando jogo --------------------------------"
+puts " Digite o numero de rodadas que deseja jogar: [1-10]"
+rodadas = gets.chomp.to_i
+rodadas.times do |i|
+  puts "Digite os numeros correspondente as cores separados por virgula! [1-9]"
+  cor =  gets.chomp.split(",").map(&:to_i)
+  puts cor
+  master.verifica_cor_posicao(cor,@cor)
+end
